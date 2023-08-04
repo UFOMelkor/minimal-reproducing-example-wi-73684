81f0a6e394b08a6d52a42bfa953b42b667581f91
Oskar Pfeifer-Bley <oskar@programming-php.net>
2023-08-04 07:55:30 +0200

    Provide minimal reproducible example

---
+++ src/a.spec.ts
@@ -0,0 +1,7 @@
+import {a} from "./a";
+
+describe('a', () => {
+    it("should be 1", () => {
+        expect(a).toBe(1);
+    });
+});
---
+++ src/b.spec.ts
@@ -0,0 +1,7 @@
+import {b} from "b";
+
+describe("b", () => {
+    it("should be 2", () => {
+        expect(b).toBe(2);
+    });
+});
